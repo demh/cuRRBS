612732e562efceacec109df90b912185c524b8fb
Daniel E. Martin-Herranz <dem44@ebi.ac.uk>
2017-02-13 11:41:49 +0100

    Changes in doc and messaging

--- src/calculate_SNE_nondeterministic.R
+++ src/calculate_SNE_nondeterministic.R
@@ -2,19 +2,21 @@
 #########      cuRRBS: customized Reduced Representation Bisulfite Sequencing     #########
 ###########################################################################################
 #
-# Created by Daniel E. Martin-Herranz and Thomas Stubbs.
+# Created by Daniel E. Martin-Herranz, Antonio J.M. Ribeiro and Thomas M. Stubbs.
 #
-# Copyright (C) 2016,2017 D.E. Martin-Herranz, T. Stubbs.
+# Copyright (C) 2016,2017 D.E. Martin-Herranz, A.J.M. Ribeiro, T.M. Stubbs.
 #
-# This program is free software: you can redistribute it and/or modify it under the terms 
+# This file is part of cuRRBS.
+#
+# cuRRBS is free software: you can redistribute it and/or modify it under the terms 
 # of the GNU General Public License as published by the Free Software Foundation, either 
 # version 3 of the License, or (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+# cuRRBS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
 # without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
 # See the GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License along with this program. 
+# You should have received a copy of the GNU General Public License along with cuRRBS. 
 # If not, see <http://www.gnu.org/licenses/>.
 #
 ###########################################################################################
