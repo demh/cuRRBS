236333d2a69bb31831069e844e55ff298b19034b
Daniel E. Martin-Herranz <dem44@ebi.ac.uk>
2017-06-19 00:02:20 +0100

    Modify paper files

--- paper/Figure_1/Figure_1B_C/Annotation_files/old_stuff/GC_content/calculate_GC_content.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python create_windows.py chr_lengths.txt 50
-
-bedtools nuc -fi /nfs/nobackup/thornton/dem44/bismark_genomes/human_hg38/hg38.fa -bed windows_50_bp.gff | awk -v OFS='\t' '{print $1, $2, $3, $4, $5, $11, $7, $8, $9}'| awk '{if(NR>1)print}' > GC_content_hg38_50bp.gff
